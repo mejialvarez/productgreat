e0cbe02ed8e501768ee5c1ecfe24d545cb3bb0f4
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2015-09-14 18:08:24 -0500

    Implements the functionality of authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   resources :users, only: [:new, :create]
   resources :products
 end
